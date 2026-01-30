0ef6e50a876460de1af9c178f2a1839324d7f67f
MohamedNumair <Mo7amednumair@gmail.com>
2026-01-30 03:11:26 +0100

    test mock example to see if it show in docs

--- Examples/example.jl
+++ Examples/example.jl
@@ -1,2 +1,10 @@
 using Pkg
 Pkg.add(PackageSpec(path=pwd()))
+
+using Pliers
+using PowerModelsDistribution
+
+
+# parse dss file
+
+dss_file = joinpath(@__DIR__, "..", "examples", "IEEE_13_Node_Test_Feeder.dss")
